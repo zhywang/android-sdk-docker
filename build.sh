4165ce6d57ee222af87702315b35c9fd71ef4610
Wang Zhiyong <zhywang@live.com>
2021-03-04 12:28:27 +0800

    Switch to jib, reduce docker dependencies. 6858069-29-29.0.3.

--- build.sh
+++ build.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 set -e
-ANDROID_SDK_VERSION="4333796"
-ANDROID_VERSION="28"
-ANDROID_BUILD_TOOLS_VERSION="28.0.3"
-GRADLE_VERSION="4.10.1"
+ANDROID_SDK_VERSION="6858069"
+ANDROID_VERSION="29"
+ANDROID_BUILD_TOOLS_VERSION="29.0.3"
+GRADLE_VERSION="6.8.3"
 NAME="zhywang/android-sdk"
 TAG="$ANDROID_SDK_VERSION-$ANDROID_VERSION-$ANDROID_BUILD_TOOLS_VERSION"
-sed -e "s/EMAIL/$email/;s/AUTH/$auth/" templ > config.json
-docker build --build-arg ANDROID_SDK_VERSION=$ANDROID_SDK_VERSION --build-arg ANDROID_VERSION=$ANDROID_VERSION --build-arg ANDROID_BUILD_TOOLS_VERSION=$ANDROID_BUILD_TOOLS_VERSION --build-arg GRADLE_VERSION=$GRADLE_VERSION -t "$NAME" .
-docker tag $NAME $NAME:$TAG
-docker --config=. push "$NAME:$TAG"
-rm -rf config.json
+
+#prepare android-sdk-linux
+mkdir android-sdk-linux
+wget -q https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_VERSION}_latest.zip
+unzip -qq commandlinetools-linux-${ANDROID_SDK_VERSION}_latest.zip
+rm -rf commandlinetools-linux-${ANDROID_SDK_VERSION}_latest.zip
+mv cmdline-tools/* android-sdk-linux/
+yes | android-sdk-linux/bin/sdkmanager --sdk_root=./android-sdk-linux "platform-tools" "build-tools;$ANDROID_BUILD_TOOLS_VERSION" "platforms;android-$ANDROID_VERSION"
+
+wget -q https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
+unzip -qq gradle-${GRADLE_VERSION}-bin.zip
+rm -rf gradle-${GRADLE_VERSION}-bin.zip
+mv gradle-${GRADLE_VERSION} gradle
+mkdir -p gradle/init.d
+cp init.gradle gradle/init.d/init.gradle
+
+wget -q https://github.com/GoogleContainerTools/jib/releases/download/v0.2.0-cli/jib-jre-0.2.0.zip
+unzip -qq jib-jre-0.2.0.zip
+rm -rf jib-jre-0.2.0.zip
+JIB=jib-0.2.0/bin/jib
+$JIB build --username "$user" --password "$password" -b jib.yaml -t registry.hub.docker.com/$NAME:$TAG
